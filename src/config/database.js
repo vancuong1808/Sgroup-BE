85cc357c80b1127cdfe1e61fb34ef9eeea62d7c2
vancuong1808 <vancuong1808@gmail.com>
2024-07-18 09:48:34 +0700

    update config database

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,8 @@ const pool = mysql2.createPool({
     host: process.env.LOCAL_HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 }).promise();
 
 ( async() => {
