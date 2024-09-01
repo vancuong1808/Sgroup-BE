1294b2b078be3b76314d3707afd4668b048a510b
vancuong1808 <vancuong1808@gmail.com>
2024-09-01 15:38:51 +0700

    fix app.js

--- app.js
+++ app.js
@@ -4,33 +4,14 @@ import routes from './src/routes/index.routes.js'
 import responseHandler from './src/handlers/response.handler.js';
 dotenv.config();
 const app = express();
-
-app.use( express.json() );
-app.use( "/api", routes );
-
-<<<<<<< Updated upstream
-// const excute = async() => {
-//     const users = await UserService.getAllUsers();
-//     console.log( users );
-//     const UserName = "Doe";
-//     const Password = "123";
-//     const login = await LoginService.Login( UserName, Password );
-//     console.log( login );
-// }
-
-
-
-
 app.use( express.json() )
 app.use("/api/user", SuserRoute );
 app.use("/api/auth", AuthSuserRoute );
 app.use("/api", ApiVoteRoute );
 
-=======
 app.use( (error, req, res, next ) => {
     responseHandler.error(res, error);
 });
->>>>>>> Stashed changes
 
 app.listen( process.env.PORT || 3000 , function( req, res ) {
     console.log('Example app listening on port 3000!');
